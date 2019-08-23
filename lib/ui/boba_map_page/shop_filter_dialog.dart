1ac1771a17c1e39a418471989beed6e69fc6e55f
billdai-dev <billdai.dev@gmail.com>
2019-08-23 13:08:53 +0800

    feat(ShopFilterDialog): Add ScaleAnimation for dialog

--- lib/ui/boba_map_page/shop_filter_dialog.dart
+++ lib/ui/boba_map_page/shop_filter_dialog.dart
@@ -11,10 +11,12 @@ class ShopFilterDialog extends StatefulWidget {
   _ShopFilterDialogState createState() => _ShopFilterDialogState();
 }
 
-class _ShopFilterDialogState extends State<ShopFilterDialog> {
+class _ShopFilterDialogState extends State<ShopFilterDialog>
+    with SingleTickerProviderStateMixin {
   AppBloc appBloc;
   ValueNotifier<bool> isAllCheckedNotifier;
   ValueNotifier<Set<String>> filteredShopsNotifier;
+  AnimationController _animController;
 
   @override
   void initState() {
@@ -22,6 +24,11 @@ class _ShopFilterDialogState extends State<ShopFilterDialog> {
     appBloc = Provider.of<AppBloc>(context, listen: false);
     isAllCheckedNotifier = ValueNotifier(widget._filteredShops.isEmpty);
     filteredShopsNotifier = ValueNotifier(Set.of(widget._filteredShops ?? {}));
+    _animController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    _animController.forward();
   }
 
   @override
@@ -30,127 +37,87 @@ class _ShopFilterDialogState extends State<ShopFilterDialog> {
     double widthMargin = screenWidth * 0.05 / 2;
     double screenHeight = MediaQuery.of(context).size.height;
     double heightMargin = screenHeight * 0.2 / 2;
-    return Container(
-      margin:
-          EdgeInsets.symmetric(horizontal: widthMargin, vertical: heightMargin),
-      child: Dialog(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
+    return WillPopScope(
+      onWillPop: () async {
+        await _animController.reverse();
+        return true;
+      },
+      child: ScaleTransition(
+        scale: Tween(begin: 0.0, end: 1.0).animate(
+          CurvedAnimation(
+            parent: _animController,
+            curve: Curves.fastOutSlowIn,
+          ),
         ),
         child: Container(
-          padding: const EdgeInsets.fromLTRB(20, 16, 20, 0),
-          child: StreamBuilder<List<String>>(
-            stream: appBloc.supportedShops
-                .map((shops) => shops.map((shop) => shop.name).toList()),
-            builder: (context, supportedShopsData) {
-              List<String> supportedShops = supportedShopsData.hasData
-                  ? supportedShopsData.data.toList()
-                  : [];
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Container(
-                    child: Text(
-                      "選擇你想找的飲料店",
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                  ),
-                  Divider(height: 16),
-                  ValueListenableBuilder<Set<String>>(
-                    valueListenable: filteredShopsNotifier,
-                    builder: (context, filteredShops, child) {
-                      bool isAllChecked = isAllCheckedNotifier.value;
-                      if (filteredShops.isNotEmpty) {
-                        for (var shop in supportedShops) {
-                          isAllChecked &= filteredShops.contains(shop);
-                        }
-                      }
-                      isAllCheckedNotifier.value = isAllChecked;
-                      return _buildCheckbox(isAllChecked, "全選", (isChecked) {
-                        if (isChecked) {
-                          filteredShopsNotifier?.value = {};
-                        } else {
-                          //Add a fake item to deselect all
-                          filteredShopsNotifier?.value = {""};
-                        }
-                        isAllCheckedNotifier.value = isChecked;
-                      });
-                    },
-                  ),
-                  Expanded(
-                    child: _buildSupportedShopGrid(supportedShops),
-                  ),
-                  Row(
+          margin: EdgeInsets.symmetric(
+              horizontal: widthMargin, vertical: heightMargin),
+          child: Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(20, 16, 20, 0),
+              child: StreamBuilder<List<String>>(
+                stream: appBloc.supportedShops
+                    .map((shops) => shops.map((shop) => shop.name).toList()),
+                builder: (context, supportedShopsData) {
+                  List<String> supportedShops = supportedShopsData.hasData
+                      ? supportedShopsData.data.toList()
+                      : [];
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: <Widget>[
-                      Expanded(
-                        child: FlatButton(
-                          onPressed: () => Navigator.pop(context),
-                          child:
-                              Text("取消", style: TextStyle(color: Colors.grey)),
+                      Container(
+                        child: Text(
+                          "選擇你想找的飲料店",
+                          style: Theme.of(context).textTheme.title,
                         ),
                       ),
+                      Divider(height: 16),
+                      buildAllCheckedButton(supportedShops),
                       Expanded(
-                        child: ValueListenableBuilder<Set<String>>(
-                          valueListenable: filteredShopsNotifier,
-                          builder: (context, filters, child) {
-                            return ValueListenableBuilder<bool>(
-                              valueListenable: isAllCheckedNotifier,
-                              builder: (context, isAllChecked, child) {
-                                final filters = filteredShopsNotifier.value;
-                                bool isDisabled = !isAllChecked &&
-                                    (filters.isEmpty ||
-                                        filters.length == 1 &&
-                                            filters.contains(""));
-                                return FlatButton(
-                                  textColor: Colors.blueAccent,
-                                  disabledTextColor: Colors.grey.shade700,
-                                  onPressed: isDisabled
-                                      ? null
-                                      : () {
-                                          final filters =
-                                              filteredShopsNotifier.value;
-                                          if (filters.isEmpty) {}
-                                          return Navigator.pop(context,
-                                              filteredShopsNotifier.value);
-                                        },
-                                  child: child,
-                                );
-                              },
-                              child: Text(
-                                "確定",
-                              ),
-                            );
-                          },
-                        ),
-                      )
+                        child: _buildSupportedShopGrid(supportedShops),
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: buildCancelButton(context),
+                          ),
+                          Expanded(
+                            child: buildConfirmButton(),
+                          )
+                        ],
+                      ),
                     ],
-                  ),
-                ],
-              );
-            },
+                  );
+                },
+              ),
+            ),
           ),
         ),
       ),
     );
   }
 
-  Widget _buildCheckbox(
-      bool isChecked, String title, Function(bool) onChanged) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Checkbox(
-          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-          value: isChecked,
-          onChanged: onChanged,
-        ),
-        Expanded(
-          child: GestureDetector(
-            onTap: () => onChanged(!isChecked),
-            child: Text(title),
-          ),
-        ),
-      ],
+  Widget buildAllCheckedButton(List<String> supportedShops) {
+    return ValueListenableBuilder<bool>(
+      valueListenable: isAllCheckedNotifier,
+      builder: (context, isAllChecked, child) {
+        final filters = Set.of(filteredShopsNotifier.value);
+        if (filters.isNotEmpty) {
+          isAllChecked = true;
+          for (var shop in supportedShops) {
+            isAllChecked &= filters.contains(shop);
+          }
+        }
+        return _buildCheckbox(isAllChecked, "全選", (isChecked) {
+          if (isChecked) {
+            filteredShopsNotifier?.value = {};
+          }
+          isAllCheckedNotifier.value = isChecked;
+        });
+      },
     );
   }
 
@@ -180,11 +147,10 @@ class _ShopFilterDialogState extends State<ShopFilterDialog> {
                         final newFilterList =
                             Set.of(filteredShopsNotifier.value);
                         if (isChecked) {
-                          //Remove fake item added by "Select all" checkbox if exists
-                          newFilterList.remove("");
                           newFilterList.add(shopName);
                         } else {
                           if (isAllChecked) {
+                            //The set will exclude shops already in the filter list
                             newFilterList.addAll(supportedShops);
                           }
                           newFilterList.remove(shopName);
@@ -202,6 +168,69 @@ class _ShopFilterDialogState extends State<ShopFilterDialog> {
           ),
         );
       },
+    );
+  }
+
+  FlatButton buildCancelButton(BuildContext context) {
+    return FlatButton(
+      onPressed: () async {
+        await _animController?.reverse();
+        return Navigator.pop(context);
+      },
+      child: Text("取消", style: TextStyle(color: Colors.grey)),
+    );
+  }
+
+  ValueListenableBuilder<Set<String>> buildConfirmButton() {
+    return ValueListenableBuilder<Set<String>>(
+      valueListenable: filteredShopsNotifier,
+      builder: (context, filters, child) {
+        return ValueListenableBuilder<bool>(
+          valueListenable: isAllCheckedNotifier,
+          builder: (context, isAllChecked, child) {
+            final filters = filteredShopsNotifier.value;
+            bool isDisabled = !isAllChecked &&
+                (filters.isEmpty ||
+                    filters.length == 1 && filters.contains(""));
+            var onPressed;
+            if (!isDisabled) {
+              onPressed = () async {
+                await _animController?.reverse();
+                return Navigator.pop(context, filteredShopsNotifier.value);
+              };
+            }
+            return FlatButton(
+              textColor: Colors.blueAccent,
+              disabledTextColor: Colors.grey.shade700,
+              onPressed: onPressed,
+              child: child,
+            );
+          },
+          child: Text(
+            "確定",
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildCheckbox(
+      bool isChecked, String title, Function(bool) onChanged) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Checkbox(
+          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          value: isChecked,
+          onChanged: onChanged,
+        ),
+        Expanded(
+          child: GestureDetector(
+            onTap: () => onChanged(!isChecked),
+            child: Text(title),
+          ),
+        ),
+      ],
     );
   }
 }
